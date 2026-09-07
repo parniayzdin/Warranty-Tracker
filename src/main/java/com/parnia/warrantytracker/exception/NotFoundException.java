33c14690605f5148372dfb78e0c01948893fb268
Parnia Yazdinia <151248827+parniayzdin@users.noreply.github.com>
2026-09-07 18:57:43 -0400

    Ensure missing resources return 404

--- src/main/java/com/parnia/warrantytracker/exception/NotFoundException.java
+++ src/main/java/com/parnia/warrantytracker/exception/NotFoundException.java
@@ -1,6 +1,11 @@
 package com.parnia.warrantytracker.exception;
 
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String resource) { super(resource + " not found"); }
-}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String resource) {
+        super(resource + " not found");
+    }
+}
